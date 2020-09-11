cebed88c2d2a0a59092ece7a31750d32154e1b40
Daniel Murphy <danielmichaelmurphy@gmail.com>
2020-09-10 23:22:21 -0400

    Added initialize method with instance variable to top

--- player.rb
+++ player.rb
@@ -4,16 +4,22 @@
 class Player
   attr_reader :inputted_code
 
+  def initialize
+    @inputted_code = []
+  end
+
+
   def enter_code
     loop do
       puts 'Enter 4 numbers between 1 - 6. Do not use spaces or commas. E.g. 1234'
       @inputted_code = gets.chomp.each_char.map { |c| c.to_i }
-      break if check_valid
+      break if check_valid?
+
       puts "That's not valid."
     end
   end
 
-  def check_valid
+  def check_valid?
     @inputted_code.all? { |i| i.is_a?(Integer) && i.between?(1, 6) } && @inputted_code.length == 4
   end
 end
